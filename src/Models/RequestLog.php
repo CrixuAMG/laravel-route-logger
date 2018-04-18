8d1cab5042665f5fe9a4f1179e2f3962042a8714
Unknown <christiankaal@gmail.com>
2018-04-18 21:10:36 +0200

    Added model and migration

---
+++ src/Models/RequestLog.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace CrixuAMG\RouteLogger\Models;
+
+class RequestLog extends Model
+{
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'parameters' => 'object',
+        'query'      => 'object',
+    ];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'uri',
+        'method',
+        'parameters',
+        'query',
+        'ip',
+    ];
+    /**
+     * @var string 
+     */
+    protected $table = 'request_logs';
+}
